9e04bc5c3ae702141c4ab54f80957c72b31ea4bf
austin chung <achung13@illinois.edu>
2016-08-31 01:16:32 -0500

    deleted duplicate question

--- hw1.py
+++ hw1.py
@@ -378,59 +378,6 @@ def mom_and_dad(string):
 
 
 
-###############################################################################
-# Pattern Printing
-#
-# You are given two strings. One
-# as the continous substring 'dad'. Otherwise, RETURN false.
-#
-# The input will always be given as a lower case string. You do not have to worry about upper cases.
-#
-# Example(s)
-# ----------
-# Example 1:
-#   Input: mom_and_dad('momdad')
-#   We have the substring going from 'mom' and 'dad'. There is one each.
-#
-#   Output:
-#   True
-#
-# Example 2:
-#   Input: mom_and_dad('momomdad')
-#   We have the string 'mom' from the first part of the string (mom omdad)
-#   However, we have another occurance of 'mom' right after. (mo mom dad)
-#   Next we only have 1 occurance of 'dad'.
-#
-#   Output:
-#   False
-#
-# Parameters
-# ----------
-# string: String
-#   String representing the sentence to find the number of occurances of 'mom' and 'dad'.
-#   The string will always be given as a lowercase.
-#
-# Returns
-# -------
-# Bool
-#   Boolean representing whether or not the same number of 'mom' and 'dad' occur in the string.
-def mom_and_dad(string):
-    """
-    Given a string, RETURN True if the number of appearnces of 'mom' and 'dad' are the same.
-
-    >>> mom_and_dad('momdad')
-    True
-    >>> mom_and_dad('momomdad')
-    False
-    >>> mom_and_dad('mom091213aiomomdadmomoomomomdadadfishsdadandwich')
-    False
-    >>> mom_and_dad('momomdadad')
-    True
-    """
-    # Your code goes here!
-    pass  # Remove me after starting
-
-
 if __name__ == '__main__':
     # Students can test this just by running the script
     # With the -v argument, they can find out the ones they got wrong.
